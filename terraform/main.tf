ddc1aaa1625e4603b55cd474c15e5db3ea3ac123
gaurav152002 <gauravjith1015@gmail.com>
2026-02-26 15:01:32 +0530

    Task11 clean infra baseline new

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,8 @@
 # NETWORK MODULE CALL
 #############################################
 
-module "network" {
-  source = "./modules/network"
+module "vpc" {
+  source = "./modules/vpc"
 
   vpc_cidr              = var.vpc_cidr
   public_subnet_1_cidr  = var.public_subnet_1_cidr
